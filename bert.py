7c449eac9580a961c347b8c5ad2f71f2623ab47f
Ryan Napolitano <ryannapp@gmail.com>
2021-03-24 13:37:17 -0400

    Added eval_msr

--- bert.py
+++ bert.py
@@ -2,6 +2,11 @@ import torch
 import numpy as np
 from scipy.stats import spearmanr
 from transformers import AutoTokenizer, AutoModel
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from scipy.spatial.distance import cosine
+from process import load_msr
+
 
 def get_word_vectors(text, model, tokenizer):
     '''
@@ -47,6 +52,28 @@ def eval_wordsim(model, tokenizer, f='data/wordsim353/combined.tab'):
 
     return spearmanr(sim, pred)
 
+def eval_msr(model):
+    '''
+        Evaluates a trained embedding model on the MSR paraphrase task using
+        logistic regression over cosine similarity scores.
+    '''
+    X_tr, y_tr = load_msr('data/msr/msr_paraphrase_train.txt')
+    X_test, y_test = load_msr('data/msr/msr_paraphrase_test.txt')
+
+    train = [[np.sum(get_word_vectors(" ".join(ss[0]), model, tokenizer).values(), axis=0),
+              np.sum(get_word_vectors(" ".join(ss[1]), model, tokenizer).values(), axis=0)] for ss in X_tr]
+    test = [[np.sum(get_word_vectors(" ".join(ss[0]), model, tokenizer).values(), axis=0),
+             np.sum(get_word_vectors(" ".join(ss[1]), model, tokenizer).values(), axis=0)] for ss in X_test]
+
+    tr_cos = np.array([1 - cosine(x[0], x[1]) for x in train]).reshape(-1, 1)
+    test_cos = np.array([1 - cosine(x[0], x[1]) for x in test]).reshape(-1, 1)
+
+    lr = LogisticRegression(class_weight='balanced', solver='liblinear')
+    lr.fit(tr_cos, y_tr)
+    preds = lr.predict(test_cos)
+
+    return accuracy_score(y_test, preds)
+
 if __name__ == "__main__":
 
     print('[evaluate] Loading model...')
@@ -54,14 +81,14 @@ if __name__ == "__main__":
     model = AutoModel.from_pretrained('bert-base-uncased')
     model.eval()
 
-    print('[evaluate] WordSim353 correlation:')
-    ws = eval_wordsim(model, tokenizer)
-    print(ws)
+    # print('[evaluate] WordSim353 correlation:')
+    # ws = eval_wordsim(model, tokenizer)
+    # print(ws)
 
     # print('[evaluate] BATS accuracies:')
     # bats = eval_bats(model, matrix, vocab, indices)
     # print(bats)
-    #
-    # print('[evaluate] MSR accuracy:')
-    # msr = eval_msr(model)
-    # print(msr)
+
+    print('[evaluate] MSR accuracy:')
+    msr = eval_msr(model)
+    print(msr)
