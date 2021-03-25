c8627e5f3bc82ea4a0591d38716d825fe49a179e
Ryan Napolitano <ryannapp@gmail.com>
2021-03-25 13:40:58 -0400

    Adjusted Flags to True

--- solution.py
+++ solution.py
@@ -27,8 +27,8 @@ RESULTS_PREFIX = "results"
 # Word2Vec run settings
 W2V_PREFIX = "w2v"
 W2V_EXT = "bin"
-GENERATE_W2V_MODELS = False
-EVALUATE_W2V_MODELS = False
+GENERATE_W2V_MODELS = True
+EVALUATE_W2V_MODELS = True
 EPOCHS = 30  # TODO: Adjust Epochs
 
 # svd run settings
